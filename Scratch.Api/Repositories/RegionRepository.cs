69a4ee37fe187678584a1d6842db0703d816b9ae
TusharHulle96 <tushar.hulle96@gmail.com>
2022-12-21 13:25:22 +0530

    End Of section 4

--- Scratch.Api/Repositories/RegionRepository.cs
+++ Scratch.Api/Repositories/RegionRepository.cs
@@ -14,7 +14,27 @@ namespace Scratch.Api.Repositories
             this.nzWalksDbContext = nzWalksDbContext;
         }
 
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id =Guid.NewGuid();
+            await nzWalksDbContext.AddAsync(region);
+            await nzWalksDbContext.SaveChangesAsync();
+            return region;
+        }
 
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await nzWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+          
+            if (region == null)
+            {
+                return null;
+            }
+
+            nzWalksDbContext.Regions.Remove(region);
+            nzWalksDbContext.SaveChangesAsync();
+            return region;
+        }
 
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
@@ -22,6 +42,33 @@ namespace Scratch.Api.Repositories
         //IEnumerable<Region> IRegionRepository.GetAll()
         //{
             return await nzWalksDbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+           return await nzWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await nzWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            existingRegion.Code =region.Code; 
+            existingRegion.Name =region.Name;
+            existingRegion.Area =region.Area;
+            existingRegion.Lat  = region.Lat;
+            existingRegion.Long = region.Long;
+            existingRegion.Population = region.Population;
+
+            await nzWalksDbContext.SaveChangesAsync();
+
+            return existingRegion;
+
         }
     }
 }
